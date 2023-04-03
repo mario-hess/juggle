e9886818dc3cf99667d78c9b01035e9127dba99f
Mario Hess <office.hess@gmail.com>
2023-04-03 15:38:52 +0200

    :art: Basic Navbar styling

--- src/components/MusicPlayer/Navbar/Navbar.jsx
+++ src/components/MusicPlayer/Navbar/Navbar.jsx
@@ -1,22 +1,52 @@
 import React from 'react'
-
+import { AiFillHome, AiOutlineUnorderedList } from "react-icons/ai";
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
   width: 100%;
-  background: lightgrey;
+  background: #fafafa;
+  padding: 1rem;
+`
+
+const Heading = styled.h3`
+text-align: left;
+
+`
+
+const MenuWrapper = styled.div`
+  margin-top: 2rem;
+`
+
+const Menu = styled.p`
+  color: lightgrey;
+  text-align:left;
+`
+
+const LinkWrapper = styled.div`
+  margin: 1rem;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+    color: ${(props) => props.theme.colors.accent};
+  }
+ 
+`
+
+const Link = styled.a`
+  margin-left: 1rem;
 `
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <p>Menu</p>
-      <a>Home</a>
-      <a>PlayLists</a>
-      <p>Playlists</p>
-      <a>Playlist 1</a>
-      <a>Playlist 2</a>
-      <a>Playlist 3</a>
+      <Heading>Juggle</Heading>
+      <MenuWrapper>
+      <Menu>Menu</Menu>
+      <LinkWrapper><AiFillHome/><Link>Home</Link></LinkWrapper>
+      <LinkWrapper><AiOutlineUnorderedList /><Link>Playlists</Link></LinkWrapper>
+      </MenuWrapper>
     </Wrapper>
   )
 }
